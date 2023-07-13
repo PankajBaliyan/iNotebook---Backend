227d6d7451bf1d91fc0b6009c0e16c6162c23fff
Pankajkumar90 <pankajbaliyan90@gmail.com>
2023-07-14 01:27:38 +0530

    fix http proxy

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const express = require('express');
-const connectToMongo = require('./db');
-const path = require('path');
+// const express = require('express');
+// const connectToMongo = require('./db');
 
-//set dotenv
+// //set dotenv
+// require('dotenv').config();
+// const app = express();
+// const port = process.env.PORT;
+// const mongoURI = process.env.MONGOURI;
+
+// //cors setup
+// const cors = require('cors');
+// app.use(cors());
+
+// connectToMongo(mongoURI);
+
+// app.use(express.json());
+
+// //Available routes
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/notes', require('./routes/notes'));
+
+// app.listen(port, () => {
+//     console.log(`Server => http://localhost:${port}`);
+// });
+
+const express = require('express');
+const httpProxy = require('http-proxy');
+const connectToMongo = require('./db');
+
+// set dotenv
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 const mongoURI = process.env.MONGOURI;
 
-//cors setup
+// cors setup
 const cors = require('cors');
 app.use(cors());
 
@@ -16,13 +41,32 @@ connectToMongo(mongoURI);
 
 app.use(express.json());
 
-//Define all catch routes
-app.use(express.static(__dirname));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+// Reverse proxy setup
+const proxy = httpProxy.createProxyServer();
+
+app.use((req, res, next) => {
+    // Conditionally determine the target URL based on the request
+    let targetURL = 'https://www.example.com'; // Replace with your frontend URL
+
+    // Update the targetURL based on the request's path
+    if (req.path.startsWith('/api')) {
+        // If the request starts with '/api', forward it to the backend
+        targetURL = `http://localhost:${port}`;
+    }
+
+    // Proxy the request to the appropriate target URL
+    proxy.web(req, res, { target: targetURL }, (err) => {
+        console.error('Proxy error:', err);
+        res.status(500).send('Proxy error');
+    });
 });
 
-//Available routes
+// Proxy error handling
+proxy.on('error', (err) => {
+    console.error('Proxy error:', err);
+});
+
+// Available routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
