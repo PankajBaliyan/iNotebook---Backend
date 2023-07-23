e91a5370aa0200fbaf7ee684db2d2e80196a5d7f
Pankajkumar90 <pankajbaliyan90@gmail.com>
2023-07-24 01:26:39 +0530

    revert to old code while fixing bug

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const connectToMongo = require('./db');
-const path = require('path');
 
 // Set dotenv
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000; // Use the appropriate port for your backend
+const port = process.env.PORT || 3001; // Use the appropriate port for your backend
 const mongoURI = process.env.MONGOURI;
 
 // CORS setup
@@ -19,16 +18,6 @@ app.use(express.json());
 // Available routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
-
-// Serve the React app's build files
-app.use(express.static(path.join(__dirname, 'https://inotebook-0ybd.onrender.com')));
-console.log("hi")
-// Wildcard route to serve the React app's index.html for all other routes
-app.get('*', (req, res) => {
-    res.sendFile(
-        path.join(__dirname, 'https://inotebook-0ybd.onrender.com', 'index.html'),
-    );
-});
 
 app.listen(port, () => {
     console.log(`Server => http://localhost:${port}`);
