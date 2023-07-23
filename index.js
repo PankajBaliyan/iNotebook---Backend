3535065cfdcf27302fd9319941e35e5eaf6d5f27
Pankajkumar90 <pankajbaliyan90@gmail.com>
2023-07-24 00:57:44 +0530

    handle refresh issue

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const connectToMongo = require('./db');
+const path = require('path');
 
-//set dotenv
+// Set dotenv
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000; // Use the appropriate port for your backend
 const mongoURI = process.env.MONGOURI;
 
-//cors setup
+// CORS setup
 const cors = require('cors');
 app.use(cors());
 
@@ -15,9 +16,17 @@ connectToMongo(mongoURI);
 
 app.use(express.json());
 
-//Available routes
+// Available routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
+
+// Serve the React app's build files from the frontend's public URL
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Wildcard route to serve the React app's index.html for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server => http://localhost:${port}`);
